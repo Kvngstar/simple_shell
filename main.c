78f29a9ff8cafcee23333138091dabe009fe27ab
kvngstar <kvngsley019@gmail.com>
2023-10-20 22:42:11 +0100

    commited

--- main.c
+++ main.c
@@ -7,72 +7,77 @@
  *
  * Return: 0 on success, or an error code if something goes wrong.
  */
-int main(int argc, char **argv) {
-    char **storeInputs;
-    int arguCount;
-  int checkpipe = 0;
-  if (isatty(fileno(stdin))) {
-     checkpipe++;
-      
-  }
+int main(int argc, char **argv)
+{
+char **storeInputs;
+int arguCount;
+int checkpipe = 0;
 
-    if (argc < 2) {
-        while (1) {
-            char *inputText = getLineInput(NULL);
-  
-            if (inputText != NULL) {
-                // Tokenize the input
+if (isatty(fileno(stdin)))
+		checkpipe++;
+
+if (argc < 2)
+{
+    while (1)
+	{
+        char *inputText = getLineInput(NULL);
+            if (inputText != NULL)
+			{
                 Tokenize(inputText, &storeInputs, &arguCount);
-
-                // Check if the command exists
                 int exists = CheckIfCommandExists(storeInputs[0], storeInputs, arguCount);
 
-                if (exists == -1) {
+                if (exists == -1)
+				{
                     perror(storeInputs[0]);
                 }
-
-                // Clean up allocated memory
                 int freespace = 0;
-                while (freespace < arguCount) {
+
+                while (freespace < arguCount)
+				{
                     free(storeInputs[freespace]);
                     freespace++;
                 }
                 free(storeInputs);
                 free(inputText);
-              if(checkpipe == 0){
-                exit(0);
-              }
+              if(checkpipe == 0)
+                	exit(0);
             }
-            else {
+            else
+			{
                 continue;
             }
         }
-    } else if (argc > 1) {
-      
+    }
+	else if (argc > 1)
+	{
+        char *text;
         int a = 1;
         int b = 0;
         int counter = 0;
-        char *text;
 
-        while (a < argc) {
+        while (a < argc)
+		{
             counter = counter + strlen(*(argv + a));
             counter++;
             a++;
         }
-
         a = 1;
         text = malloc(sizeof(char) * (counter + 1));
         int i = 0;
-        while (a < argc) {
-            char *changetext = *(argv + a);
 
-            while (b < strlen(changetext)) {
+        while (a < argc)
+		{
+            char *changetext = *(argv + a);
+            while (b < strlen(changetext)) 
+			{
                 text[i] = *(changetext + b);
                 i++;
                 b++;
             }
             a++;
-            if (a == argc) {
+
+            if (a == argc)
+			{
                 text[i] = '\0';
                 break;
             }
@@ -81,25 +86,22 @@ int main(int argc, char **argv) {
             text[i] = ' ';
             i++;
         }
-     
         Tokenize(text, &storeInputs, &arguCount);
-
         int exists = CheckIfCommandExists(storeInputs[0], storeInputs, arguCount);
 
-        if (exists == -1) {
+        if (exists == -1)
+		{
             perror(storeInputs[0]);
         }
-
-      
         int freespace = 0;
-        while (freespace < arguCount) {
+
+        while (freespace < arguCount)
+		{
             free(storeInputs[freespace]);
             freespace++;
         }
         free(storeInputs);
-
-  
     }
 
-    return 0;
+    return (0);
 }
